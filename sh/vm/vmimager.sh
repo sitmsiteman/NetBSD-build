1b4bbe11c4726071e172889b8a903453b294323d
sitmsiteman <sitmsiteman@gmail.com>
2024-01-08 04:11:00 +0900

    fix typo

--- sh/vm/vmimager.sh
+++ sh/vm/vmimager.sh
@@ -3,7 +3,7 @@
 . ./env.sh
 
 # Create a disk image.
-gunzip -c "$OBJ/releasedir/$ARCH_MACHINE/binary/gzimg/arm64.img.gz" > "$WORKSPACE/vm/disk.img"
+gunzip -c "$OBJ/releasedir/$ARCH_MACHINE/binary/gzimg/arm64.img.gz" > "$VM/disk.img"
 
 # Make it a sparse 10 GB image.
 dd if=/dev/zero of=$VM/disk.img seek=10G bs=1 count=1
