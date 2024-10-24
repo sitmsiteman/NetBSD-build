18a01805a4357108dbdd03f67a8ff6deb1c00b92
sitmsiteman <107298363+sitmsiteman@users.noreply.github.com>
2024-10-24 16:55:21 +0900

    evbarm aarch64

--- sh/build/release_build.sh
+++ sh/build/release_build.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-INSTALL_LOG="buildlog.install"
-ERROR_LOG="buildlog.error"
-ARCH="amd64"
+INSTALL_LOG="$HOME/netbsd/buildlog.install"
+ERROR_LOG="$HOME/netbsd/buildlog.error"
+MACHINE="evbarm"
+MACHINE_ARCH="aarch64"
 JOBS=$(nproc)
-KERN="debug"
+KERN="$HOME/netbsd/GENERIC64-DEBUG-EVBARM"
 DATE=$(date)
 OBJS="$HOME/netbsd/obj"
+TOOLDIR="$HOME/netbsd/obj/tooldir.$(uname -s)-$(uname -r)-$(uname -m)"
 
 echo "Build start at $DATE"
 
@@ -14,13 +16,18 @@ echo "$DATE" >>"$INSTALL_LOG"
 echo "$DATE" >>"$ERROR_LOG"
 echo "=========================================="
 echo "Build tools" | tee -a "$INSTALL_LOG" | tee -a "$ERROR_LOG"
-time ./build.sh -U -u -O $OBJS -x -X ../xsrc -j $JOBS -m "$ARCH" tools 1>>"$INSTALL_LOG" 2>>"$ERROR_LOG"
+time ./build.sh -U -u -O $OBJS -x -X ../xsrc -j $JOBS -a "$MACHINE_ARCH" \
+       -m "$MACHINE" \
+      tools 1>>"$INSTALL_LOG" 2>>"$ERROR_LOG"
+
 echo "=========================================="
 echo "Build kernel" | tee -a "$INSTALL_LOG" | tee -a "$ERROR_LOG"
-time ./build.sh -U -u -O $OBJS -x -X ../xsrc -j $JOBS -m "$ARCH" kernel="$KERN" 1>"$INSTALL_LOG" 2>>"$ERROR_LOG"
+time ./build.sh -U -u -O $OBJS -x -X ../xsrc -j $JOBS -a "$MACHINE_ARCH" \
+        -m "$MACHINE" kernel="$KERN" 1>"$INSTALL_LOG" 2>>"$ERROR_LOG"
 echo "=========================================="
-echo "Build userland" | tee -a "$INSTALL_LOG" | tee -a "$ERROR_LOG"
-time ./build.sh -U -u -O $OBJS -x -X ../xsrc -j $JOBS -m "$ARCH" release 1>"$INSTALL_LOG" 2>>"$ERROR_LOG"
-echo "=========================================="
+# echo "Build userland" | tee -a "$INSTALL_LOG" | tee -a "$ERROR_LOG"
+# time ./build.sh -U -u -O $OBJS -x -X ../xsrc -j $JOBS -a "$MACHINE_ARCH" \
+#       -m "$MACHINE"  release 1>"$INSTALL_LOG" 2>>"$ERROR_LOG"
+# echo "=========================================="
 echo "Error Log:"
 cat "$ERROR_LOG"
