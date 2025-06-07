822befba93d020802bfe2d4809282f88fc70c891
sitmsiteman <sitmsiteman@gmail.com>
2025-06-07 17:12:37 +0900

    adjustable path

--- sh/build/release_build.sh
+++ sh/build/release_build.sh
@@ -9,6 +9,7 @@ KERN="$HOME/netbsd/GENERIC64-DEBUG-EVBARM"
 DATE=$(date)
 OBJS="$HOME/netbsd/obj"
 TOOLDIR="$HOME/netbsd/obj/tooldir.$(uname -s)-$(uname -r)-$(uname -m)"
+NETBSD_SRC="$HOME/netbsd/src"
 
 echo "Build start at $DATE"
 
@@ -16,17 +17,17 @@ echo "$DATE" >>"$INSTALL_LOG"
 echo "$DATE" >>"$ERROR_LOG"
 echo "=========================================="
 echo "Build tools" | tee -a "$INSTALL_LOG" | tee -a "$ERROR_LOG"
-time ./build.sh -U -u -O $OBJS -x -X ../xsrc -j $JOBS -a "$MACHINE_ARCH" \
+time $NETBSD_SRC/build.sh -U -u -O $OBJS -x -X ../xsrc -j $JOBS -a "$MACHINE_ARCH" \
        -m "$MACHINE" \
       tools 1>>"$INSTALL_LOG" 2>>"$ERROR_LOG"
 
 echo "=========================================="
 echo "Build kernel" | tee -a "$INSTALL_LOG" | tee -a "$ERROR_LOG"
-time ./build.sh -U -u -O $OBJS -x -X ../xsrc -j $JOBS -a "$MACHINE_ARCH" \
+time $NETBSD_SRC/build.sh -U -u -O $OBJS -x -X ../xsrc -j $JOBS -a "$MACHINE_ARCH" \
         -m "$MACHINE" kernel="$KERN" 1>"$INSTALL_LOG" 2>>"$ERROR_LOG"
 echo "=========================================="
 # echo "Build userland" | tee -a "$INSTALL_LOG" | tee -a "$ERROR_LOG"
-# time ./build.sh -U -u -O $OBJS -x -X ../xsrc -j $JOBS -a "$MACHINE_ARCH" \
+# time $NETBSD_SRC/build.sh -U -u -O $OBJS -x -X ../xsrc -j $JOBS -a "$MACHINE_ARCH" \
 #       -m "$MACHINE"  release 1>"$INSTALL_LOG" 2>>"$ERROR_LOG"
 # echo "=========================================="
 echo "Error Log:"
